fefddc86d26481730721f55f201afd62765ac223
Vaclav Rezac <vaclav.rezac@siemens.com>
2023-09-17 12:56:05 +0200

    amen

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from dash import Dash
 import dash_mantine_components as dmc
+from dash import Dash, html
 
-app = Dash(__name__)
+from utils import nerespoznivny_navigacny_panel
 
-app.layout = dmc.Text("Hello World!")
+app = Dash(__name__, use_pages=True)
 
+links = {
+    "aboutme": {"label": "About Me"},
+    "experience": {"label": "Experience"},
+    "contracts": {"label": "Contracts"},
+    "projects": {"label": "Projects"},
+    "add": {"label": "add"},
+}
+
+app.layout = html.Div([
+    nerespoznivny_navigacny_panel(links, logo="tabler:square-rounded-letter-v"),
+    html.Div(style={"margin": "48px"}),  # corrected this line
+    html.Div(id="page-content")
+])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run_server(debug=True)
